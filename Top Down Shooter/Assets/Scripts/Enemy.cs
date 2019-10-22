78583622562b4846394bcf4bf07b0bb31a17d117
Amathyst2 <Amathyst@users.noreply.github.com>
2019-10-22 13:53:26 +0100

    Started a new tutorial

---
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Transform playerPos;
+
+    private void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+     void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+
+
+
+
+
+
+
+
+
+}
+  
