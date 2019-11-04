0e5ce11e147681507d24326497d4e5b349a4065e
Amathyst2 <Amathyst@users.noreply.github.com>
2019-11-04 15:20:20 +0000

    Projectiles, enemies, player movement

--- Top Down Shooter/Assets/Scripts/Enemy.cs
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -6,15 +6,37 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     private Transform playerPos;
+    private Player player;
 
     void Start()
     {
-        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+            //player losing health
+        {
+            player.health--;
+            Debug.Log(player.health);
+            Destroy(gameObject);
+           
+        }
+
+        if (other.CompareTag("Projectile"))
+            
+        {
+            Destroy(gameObject);
+        }
+    
+        
     }
 
 }
--- Top Down Shooter/Assets/Scripts/Projectile.cs
+++ Top Down Shooter/Assets/Scripts/Projectile.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 target;
+    public float speed;
+
     void Start()
     {
-        
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    // Update is called once per frame
+    
+        
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target) < 0.2f)
+        {
+            Destroy(gameObject);
+        }
+
     }
+
 }
+
