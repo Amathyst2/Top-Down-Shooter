e28e25a88e98ab0ecfaee06346174c4176c54465
Amathyst2 <505038@chrome.escg.ac.uk>
2019-11-11 00:40:52 +0000

    Added 2 new scripts

---
+++ Top Down Shooter/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+   
+    public int startingHealth;
+    public int currentHealth;
+   
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+    public void HurtPlayer(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+    }
+}
